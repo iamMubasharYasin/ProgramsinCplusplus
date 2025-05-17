9da99670da0c8bf98ff11454bce581054938837b
Mubashar Yasin <mubasharraj012@gmail.com>
2025-05-17 11:13:16 +0500

    testing 3
    done

---
+++ testing3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueOne,valueTwo;
+	cout<<"Please Enter valueone "<<endl;
+	cin>>valueOne;
+	cout<<"Please Enter valuetwo "<<endl;
+	cin>>valueTwo;
+	
+	cout<<"The Addition of two value is "<<valueOne+valueTwo;
+}
